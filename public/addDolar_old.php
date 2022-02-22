56de376c9f14fc42159a1c95b4792f15f5f39acb
Fernando Ely <luisely@gmail.com>
2022-02-22 19:56:27 -0300

    enviando dolar app

---
+++ public/addDolar_old.php
@@ -0,0 +1,106 @@
+<?php
+/*acessar o site e pegar o valor do dolar ptax
+    $page = file_get_contents('https://dolarhoje.com/ptax/');
+    $doc = new DOMDocument();
+    $doc->loadHTML($page);
+    $spans = $doc->getElementsByTagName('b');
+    $i = 0;
+    foreach($spans as $span) {
+        // Loop entre as SPANs, procurando aquele com a classe VALUE. No SEGUNDO span armazena na variavel.
+        if ($span->getElementsByTagName('b') === int) {
+             $i++;
+             if ($i == 2) {
+                $usdptax = floatvalue($span->nodeValue);
+             }
+        }
+    } */
+	
+	
+    //$today = date("m-d-Y");
+    //$url = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial='01-02-2022'&@dataFinalCotacao='".$today."'&"."$"."top=2&"."$"."orderby=dataHoraCotacao%20desc&"."$"."format=json&"."$"."select=cotacaoVenda,dataHoraCotacao";
+	//$itens = json_decode(file_get_contents($url),true);
+	//$usdptaxBC = $itens["value"][0]["cotacaoVenda"];
+	
+	
+	//echo $url;
+    //echo $usdptaxBC;
+	//echo 'to aqui';
+
+error_reporting(E_ALL);
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+date_default_timezone_set('America/Sao_Paulo');
+
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug: " . $output . "' );</script>";
+}
+
+$today = date("d-m");
+$dataFeriados = array("01-01", "15-04", "21-04", "01-05", "16-06", "07-09", "12-10", "28-10", "02-11", "15-11", "25-12");
+
+//echo "<pre>";
+//var_dump($dataFeriados);
+//echo "</pre>";
+
+$verificaDataFeriado = function($arrayFeriados, $dataHoje){
+    if (in_array($dataHoje, $arrayFeriados)) { 
+        return true;
+    } else {
+        return false;
+    }
+};
+//echo $today;
+echo "<br>";
+
+//$isFeriado($dataFeriados,$today);
+
+
+//função para tranformar o texto 3,8999 para 3.8999 (Remover a virgula e usar PONTO e trasnformar em float)
+    function floatvalue($val){
+        $val = str_replace(",",".",$val);
+        $val = preg_replace('/\.(?=.*\.)/', '', $val);
+        return floatval($val);
+    }
+
+    debug_to_console("Verificando data...");
+    if ($verificaDataFeriado($dataFeriados,$today) == false) {
+        debug_to_console("Adicionando...");
+        $url = 'https://dolarhoje.com/ptax/';
+        $content = file_get_contents($url);
+        $first_step = explode( '<b>' , $content );
+        $second_step = explode("</b>" , $first_step[1] );
+        $dolar1 = substr($second_step[0], 3);
+        $usdptax = floatvalue($dolar1);
+
+        debug_to_console($today . " - " . $usdptax);
+        //encode para UTF8(joga a primeira letra para maiscula(Nome completo do mês, baseado no idioma))
+        $mesNomeAtual = utf8_encode(ucwords(strftime('%B'))); 
+
+        $str = file_get_contents('dolar.json');
+        $json = json_decode($str,true);
+        if(isset($usdptax)){
+            $AdditionalArray = array(
+                'ano' => date("Y") ,
+                'mes' => $mesNomeAtual ,
+                'dia' => date("d") ,
+                'valor' => round($usdptax, 4)
+                );
+                //append additional json to json file
+              $json[]=$AdditionalArray;
+              $jsonData = json_encode($json);
+    
+              file_put_contents('dolar.json', $jsonData);
+              debug_to_console("Registro adicionado com sucesso!");
+        }
+
+    } else {
+        debug_to_console("Hoje é feriado! :D");
+    }
+
+
+  ?>
+
+
--- public/index.php
+++ public/index.php
@@ -1,2 +1,197 @@
 <?php
-phpinfo();
+
+$str = file_get_contents('dolar.json');
+$json = json_decode($str, true, JSON_NUMERIC_CHECK);
+
+
+if (isset($_GET['ano'])) {
+
+    $AdditionalArray = array(
+        'ano' => $_GET['ano'],
+        'mes' => $_GET['mes'],
+        'dia' => $_GET['dia'],
+        'valor' => $_GET['valor']
+    );
+    //append additional json to json file
+    $json[] = $AdditionalArray;
+    $jsonData = json_encode($json);
+
+    file_put_contents('dolar.json', $jsonData);
+}
+
+function mediaMes($ano, $mes, $array)
+{
+    $total = 0;
+    $total_dias = 0;
+    foreach ($array as $cotacao) {
+        if ($cotacao['mes'] == $mes and $cotacao['ano'] == $ano) {
+            $total = $total + $cotacao['valor'];
+            $total_dias++;
+            $media = $total / $total_dias;
+        }
+    }
+
+    if (isset($media)) {
+        return number_format($media, 4, ',', '');
+    } else {
+        return 0;
+    }
+}
+
+$totalItens = 0;
+foreach ($json as $cotacao) {
+    $totalItens++;
+}
+
+$meses = array(
+    1 => 'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+);
+
+
+function convertMonthToNumber($monthName)
+{
+    if ($monthName == "Janeiro") {
+        $monthName = "January";
+    }
+    if ($monthName == "Fevereiro") {
+        $monthName = "February";
+    }
+    if ($monthName == "Março") {
+        $monthName = "March";
+    }
+    if ($monthName == "Abril") {
+        $monthName = "April";
+    }
+    if ($monthName == "Maio") {
+        $monthName = "May";
+    }
+    if ($monthName == "Junho") {
+        $monthName = "June";
+    }
+    if ($monthName == "Julho") {
+        $monthName = "July";
+    }
+    if ($monthName == "Agosto") {
+        $monthName = "August";
+    }
+    if ($monthName == "Setembro") {
+        $monthName = "September";
+    }
+    if ($monthName == "Outubro") {
+        $monthName = "October";
+    }
+    if ($monthName == "Novembro") {
+        $monthName = "November";
+    }
+    if ($monthName == "Dezembro") {
+        $monthName = "December";
+    }
+    return date('m', strtotime($monthName));
+}
+//echo '<pre>';
+//print_r($json);
+//echo '</pre>';
+
+// Declare month number and initialize it
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>TI Imdepa</title>
+
+
+    <!-- Bootstrap -->
+    <!-- JavaScript Bundle with Popper -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            background-color: #FFFFFF;
+            font-family: 'Inter';
+            font-size: 14px;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-6">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th scope="row">Dolar PTAX</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach (array_reverse(array_slice($json, -30, $totalItens)) as $cotacao) : ?>
+                            <tr>
+                                <td> <?php echo $cotacao['dia'] . "/" . convertMonthToNumber($cotacao['mes']) . "/" . $cotacao['ano']; ?> </td>
+                                <td> <?php printf("%.4f", $cotacao['valor']) ?> </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="col-12 col-sm-6">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Mês</th>
+                            <th><?php echo date("Y") - 1; ?></th>
+                            <th><?php echo date("Y"); ?></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($meses as $mesAtual) : ?>
+                            <tr>
+                                <td><?php echo $mesAtual ?></td>
+                                <td>
+                                    <?php
+                                    echo mediaMes(date("Y") - 1, $mesAtual, $json);
+                                    ?></td>
+                                <td>
+                                    <?php
+                                    echo mediaMes(date("Y"), $mesAtual, $json);
+                                    ?>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+                <p class="text-center">Dados atualizados automaticamente às 13:25</p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</body>
