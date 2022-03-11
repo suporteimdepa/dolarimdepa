9268d0ed9ffa890f6ec62de63c4d508845edf9e2
Fernando Ely <luisely@gmail.com>
2022-03-11 17:54:18 -0300

    pack locale

--- public/addDolar.php
+++ public/addDolar.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
-//setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-//date_default_timezone_set('America/Sao_Paulo');
+setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+date_default_timezone_set('America/Sao_Paulo');
 
 $envs = parse_ini_file('../foo.env');
 
