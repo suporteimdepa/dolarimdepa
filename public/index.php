b062592774312e39c0114ef8c883c4db260f5dc5
Fernando Ely <luisely@gmail.com>
2022-03-11 17:49:24 -0300

    pack locale

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-date_default_timezone_set('America/Sao_Paulo');
+//setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+//date_default_timezone_set('America/Sao_Paulo');
 
 $envs = parse_ini_file('../foo.env');
 
